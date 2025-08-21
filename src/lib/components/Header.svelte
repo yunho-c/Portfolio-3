48b1fcd97b918c12663d643a4addf0ab56b087d3
Yunho Cho <ycho358@gatech.edu>
2025-08-20 21:06:53 -0400

    Added mouseover glow to GScholar, GitHub

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -9,14 +9,25 @@
 	<nav class="mx-auto flex gap-4 sm:gap-6">
 		<Button variant="ghost" href="/about" class="text-sm font-medium">About</Button>
 		<Button variant="ghost" href="/projects" class="text-sm font-medium">Projects</Button>
-		<Button variant="ghost" href="https://research.yunhocho.com" class="text-sm font-medium">Research</Button>
-		<Button variant="ghost" href="https://blog.yunhocho.com" class="text-sm font-medium">Blog</Button>
+		<Button variant="ghost" href="https://research.yunhocho.com" class="text-sm font-medium"
+			>Research</Button
+		>
+		<Button variant="ghost" href="/projects" class="text-sm font-medium">Coursework</Button>
+		<!-- <Button variant="ghost" href="https://blog.yunhocho.com" class="text-sm font-medium"
+			>Blog</Button
+		> -->
 	</nav>
 	<nav class="flex gap-4 sm:gap-6">
-		<Button variant="link" href="https://scholar.google.comar/TODO" class="text-sm font-medium"
+		<Button
+			variant="link"
+			href="https://scholar.google.com/citations?user=zY_1e-AAAAAJ"
+			class="text-sm font-medium text-gray-600 transition-all hover:text-blue-500 hover:drop-shadow-[0_0_5px_rgba(59,130,246,0.1)] dark:text-gray-400 dark:hover:text-blue-400 dark:hover:drop-shadow-[0_0_5px_rgba(59,130,246,0.8)]"
 			>Google Scholar</Button
 		>
-		<Button variant="link" href="https://github.com/yunho-c" class="text-sm font-medium"
+		<Button
+			variant="link"
+			href="https://github.com/yunho-c"
+			class="text-sm font-medium text-gray-600 transition-all hover:text-purple-500 hover:drop-shadow-[0_0_5px_rgba(168,85,247,0.1)] dark:text-gray-400 dark:hover:text-purple-400 dark:hover:drop-shadow-[0_0_5px_rgba(168,85,247,0.8)]"
 			>GitHub</Button
 		>
 	</nav>
